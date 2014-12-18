6866d061d79e90d9098ec24b46e2acc6b419f135
Adam Avramov <fallenblood@gmail.com>
2014-12-18 19:05:40 +0200

    Load sample by relative path from command line.

--- src/main.c
+++ src/main.c
@@ -6,13 +6,24 @@
 #include "osc.h"
 
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 int main (int    argc,
           char * argv []) {
 
-    const char     * filename = "data/breakbeat-140bpm.wav";
-    global_state_t * context  = calloc(1, sizeof(global_state_t));
+    char path[1024];
+
+    if (getcwd(path, sizeof(path)) != NULL) {
+        MSG("Current working dir: %s", path);
+    } else {
+        FATAL("No working directory.");
+    }
+
+    strcat(path, "/");
+    strcat(path, argv[1]);
+
+    global_state_t * context = calloc(1, sizeof(global_state_t));
 
     context->clips   = calloc(INITIAL_CLIP_SLOTS, sizeof(audio_clip_t));
     context->n_clips = 0;
@@ -21,14 +32,11 @@ int main (int    argc,
 
     osc_start(context);
 
-    audio_clip_t * clip = context->clips[clip_add(context, filename)];
-
-    clip_cue_set(clip, 1, 18914);
-
-    /*printf("Press <ENTER> key to start playing...");*/
-    /*getchar();*/
-
-    /*clip_start(context, 0);*/
+    if (argv[1] == NULL) {
+        MSG("No sample path specified.");
+    } else {
+        audio_clip_t * clip = context->clips[clip_add(context, path)];
+    }
 
     while (1) usleep(10000);
 
